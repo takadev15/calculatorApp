4b07989c017826f89fb8710dd60d6c1a55b4a21a
Daffa Haryadi <daffa.hp.dh@gmail.com>
2021-02-15 17:18:48 +0700

    sync

--- lib/functional/calculator.dart
+++ lib/functional/calculator.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/foundation.dart';
 
 class calculatorbrain with ChangeNotifier {
+  String _output = "0";
+  List<double> number = [];
+  List<String> operator = [];
 
+  String get output => _output;
+
+  set output(String newOutput){
+    if (newOutput == "AC"){
+      _output = "0";
+      number.clear();
+      operator.clear();
+    }
+    else if (newOutput == "+" || 
+        newOutput == "-" || 
+        newOutput == "*" || 
+        newOutput == "/" || 
+        newOutput == "="){
+      number.add(double.parse(_output));
+      if (newOutput != "="){
+        operator.add(newOutput);
+        _output = "";
+      }
+      else {
+        runCalculator();
+        dynamic prettierNumber = number[0] == number[0].round() ? number[0].round() : number[0];
+      }
+    }
+  }
 
 }
 
